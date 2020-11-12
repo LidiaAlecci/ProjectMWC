f8bd56c0874b1d30d5f654cdcefa9d0e798cdfcb
Bianca Stan <50299044+bianca-stanca@users.noreply.github.com>
2020-11-12 11:31:12 +0100

    Working TabLayout inside home fragment
    Managed to get the TabLayout to work properly while inside the HomeFragment of the Navigation layout

--- Unsteppable2/app/src/main/java/com/example/unsteppable/ui/home/HomeFragment.java
+++ Unsteppable2/app/src/main/java/com/example/unsteppable/ui/home/HomeFragment.java
@@ -1,18 +1,25 @@
 package com.example.unsteppable.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.unsteppable.R;
+import com.example.unsteppable.ui.main.SectionsPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +30,46 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final ViewPager viewPager = root.findViewById(R.id.pager);
+        final SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this.getContext(), this.getChildFragmentManager());
+        Log.d("viewpager", String.valueOf(viewPager==null));
+
+
+
+        viewPager.setAdapter(sectionsPagerAdapter);
+        Log.d("sectionsPagerAdapter", String.valueOf(sectionsPagerAdapter==null));
+
+
+
+        //final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        TabLayout tabs = root.findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+
+//        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                sectionsPagerAdapter.getItem(tab.getPosition());
+//
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+
         return root;
     }
 }
