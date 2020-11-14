9159ffc5f60efde17e05ef14c6e9ed8cc18c1f19
samuu9760 <samuu9760@gmail.com>
2020-11-14 19:17:36 +0100

    First Version of Wheater API
    Adding permission to Intenet and Network State.
    Creating the first version of class Weather and the getter of the informations

--- Unsteppable2/app/src/main/java/com/example/unsteppable/MainActivity.java
+++ Unsteppable2/app/src/main/java/com/example/unsteppable/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.unsteppable;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
@@ -19,6 +21,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -27,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Weather API part
+        getWheatherFromApi();
 
         //TAB
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -91,5 +106,82 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Wheather API part
+    class Weather extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... address) {
+            //String... means multiple address can be send. It acts as array
+            try {
+                URL url = new URL(address[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                //Establish connection with address
+                connection.connect();
+
+                //Retrive data from url
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                //Retrive data and return it as String
+                int data = isr.read();
+                String content = "";
+                char ch;
+                while(data != -1){
+                    ch = (char) data;
+                    content += ch;
+                    data = isr.read();
+                }
+                return content;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    protected void getWheatherFromApi(){
+        String content;
+        String city = "Lugano";
+        String apiKey = "e9fcc5721ca04b00b71bebed9a78bae3";
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+
+        Weather weather = new Weather();
+        try {
+            //content = weather.execute("https://api.openweathermap.org/data/2.5/weather?q=Lugano&appid=e9fcc5721ca04b00b71bebed9a78bae3").get();
+            content = weather.execute(apiUrl).get();
+            //First we will check data is retrieve successfully or not
+            Log.i("contentData", content);
+
+            //JSON
+            JSONObject jsonObject = new JSONObject(content);
+            String weatherData = jsonObject.getString("weather");
+            Log.i("weatherData", weatherData);
+
+            //weather data is in Array
+            JSONArray array = new JSONArray(weatherData);
+
+            String main = "";
+            String description = "";
+
+            for(int i = 0; i < array.length(); i++){
+                JSONObject weatherPart = array.getJSONObject(i);
+                main = weatherPart.getString("main");
+                description = weatherPart.getString("description");
+            }
+
+            Log.i("main", main);
+            Log.i("descritpion", description);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
