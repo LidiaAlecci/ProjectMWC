9a9e4d2ecb8b09e701448a76aa1693a89e971258
samuu9760 <samuu9760@gmail.com>
2020-12-11 16:33:07 +0100

    Added last 30 days graph

--- Unsteppable2/app/src/main/java/com/example/unsteppable/ui/tabs/MonthTabFragment.java
+++ Unsteppable2/app/src/main/java/com/example/unsteppable/ui/tabs/MonthTabFragment.java
@@ -1,5 +1,6 @@
 package com.example.unsteppable.ui.tabs;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +9,29 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
 import com.example.unsteppable.R;
+import com.example.unsteppable.db.UnsteppableOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MonthTabFragment extends Fragment {
-
+    AnyChartView anyChartView;
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
@@ -24,6 +42,7 @@ public class MonthTabFragment extends Fragment {
         fragment.setArguments(bundle);
         return fragment;
     }
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -31,6 +50,62 @@ public class MonthTabFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_month_tab, container, false);
 
+        //Create the Chart
+        anyChartView = root.findViewById(R.id.monthBarChart);
+        anyChartView.setProgressBar(root.findViewById(R.id.loadingBar));
+
+        Cartesian cartesian = createColumnChart();
+        anyChartView.setBackgroundColor("#00000000");
+        anyChartView.setChart(cartesian);
         return root;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Cartesian createColumnChart() {
+        Map<String, Integer> graph_map = new TreeMap<>();
+        /*
+        String date;
+        Integer value;
+        for(int i = 0; i < 7; i++){
+            date = dateFormat.format(cal.getTime());
+            value = UnsteppableOpenHelper.getStepsByDayFromTab1(getContext(), date);
+            graph_map.put(date, value);
+            cal.add(Calendar.DATE, -1);
+        }*/
+        //graph_map.putAll(stepsByDay);
+        graph_map = UnsteppableOpenHelper.getStepsLast30Days(getContext());
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+
+        for (Map.Entry<String,Integer> entry : graph_map.entrySet())
+            data.add(new ValueDataEntry(entry.getKey(), entry.getValue()));
+
+        Column column = cartesian.column(data);
+
+        column.fill("#1EB980");
+        column.stroke("#1EB980");
+
+        column.tooltip()
+                .titleFormat("At day: {%X}")
+                .format("{%Value}{groupsSeparator: } Steps")
+                .anchor(Anchor.RIGHT_TOP);
+
+        column.tooltip()
+                .position(Position.RIGHT_TOP)
+                .offsetX(0d)
+                .offsetY(5);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+        cartesian.yScale().minimum(0);
+
+
+        cartesian.yAxis(0).title("Number of steps");
+        cartesian.xAxis(0).title("Day");
+        cartesian.background().fill("#00000000");
+        cartesian.animation(true);
+
+        return cartesian;
     }
 }
